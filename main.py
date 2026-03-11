2ec4db3b66d1977e46178b0e358049ba85769b97
Merthadam <merthadam9@gmail.com>
2026-03-11 15:02:48 +0100

    First Commit of agent-under-the-hood

--- main.py
+++ main.py
@@ -1,33 +1,6 @@
-from dotenv import load_dotenv
-
-load_dotenv()
-from langchain.agents import create_agent
-from langchain.tools import tool
-from langchain_core.messages import HumanMessage
-from langchain_openai import ChatOpenAI
-from tavily import TavilyClient
-from langchain_tavily import TavilySearch
-
-
-tavily = TavilyClient()
-
-
-
-
-
-
-
-llm = ChatOpenAI()
-tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
-
-
 def main():
     print("Hello from langchain-course!")
-    result = agent.invoke({"messages": HumanMessage(content="What are some jobs in Budapest that require langraph and langchain and are close to intern positions")})
-    # Agent always ends with the final AI message
-    print(result["messages"][-1].content)
+
 
 if __name__ == "__main__":
     main()
-
